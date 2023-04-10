f2a54af120468cc9c4723c4e556c7ea8c0cef526
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2023-04-10 16:21:11 -0700

    Create deploy.ts

---
+++ deploy/deploy.ts
@@ -0,0 +1,147 @@
+/*import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// compile command: npx hardhat compile
+// deploy command: npx hardhat deploy-zksync 
+
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the cult staking contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet("962ade0e36cfd80711fe663d194c56cc75571925a47e3c8cc378f5eba5b563bb");
+
+// Create deployer object and load the artifact of the contract you want to deploy.
+   const deployer = new Deployer(hre, wallet);
+   const artifact = await deployer.loadArtifact("ZKSHIBA");
+
+  // Estimate contract deployment fee
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const zkContract = await deployer.deploy(artifact, []);
+  console.log(zkContract.interface.encodeDeploy([]))
+  // Show the contract info.
+  const contractAddress = zkContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
+*/
+
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// compile command: npx hardhat compile
+// deploy command: npx hardhat deploy-zksync 
+
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the cult staking pool and factory contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet("962ade0e36cfd80711fe663d194c56cc75571925a47e3c8cc378f5eba5b563bb");
+
+// Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const poolArtifact = await deployer.loadArtifact("zkCultStakingPool");
+  const factoryArtifact = await deployer.loadArtifact("zkCultStakingFactory");
+
+  // CONSTRUCTOR VARIABLES
+  const nftCollection = "0x7D24d59d5968677F31Ab9c25941F6909585965AB"; // replace with actual address of NFT collection
+  const rewardToken = "0xB3d6Fb1BC452c6f87875832EEd00e315914969e6"; // actual address of reward token
+  const nftValueTokenAddr = "0x5Eb1Da89177A65d4Ab781d83E3eB9E551592183F"; // replace with actual address of NFT value token
+  const amountOfValueToken = 0; // replace with actual amount of value token
+  const nftTokenLogo = "https://example.com/nfttokenlogo.png"; //URL OF LOGO
+  const rewardTokenLogo = "https://example.com/rewardtokenlogo.png"; // URL OF REWARD TOKEN LOGO
+  const websiteURL = "https://zkcult.monster"; //URL OF WEBSITE
+  const lockDuration = 1; // REPLACE WITH ACTUAL LOCK DURATION IN DAYS
+  const rewardPerDay = 1; // REPLACE WITH ACTUAL REWARD PER DAY
+  const poolOwner = "0x8afACaec5DAd5F03cB3913eA2Ab1609D937b3ff3"; // replace with actual address of pool owner
+  const endTime = 0//Math.floor(Date.now() / 1000) + 3600; set end time to 1 hour from now
+
+  // Estimate contract deployment fee
+  const deploymentFee1 = await deployer.estimateDeployFee(poolArtifact, [
+    nftCollection,
+    rewardToken,
+    nftValueTokenAddr,
+    amountOfValueToken,
+    nftTokenLogo,
+    rewardTokenLogo,
+    websiteURL,
+    lockDuration,
+    rewardPerDay,
+    poolOwner,
+    endTime
+  ]);
+
+  const _zkCult = "0xc9d249fe7994d0f1787741264Bb161B0801B7f75";
+  const _zkCultAmount = ethers.utils.parseEther("0"); // Replace with the amount of zkCult to be used in deployment
+  const _ethAmount = ethers.utils.parseEther("0.001"); // Replace with the amount of Ether to be used in deployment
+
+  const deploymentFee2 = await deployer.estimateDeployFee(factoryArtifact, [
+    _zkCult,
+    _zkCultAmount,
+    _ethAmount
+  ]);
+
+  //Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  const parsedFee1 = ethers.utils.formatEther(deploymentFee1.toString());
+  const parsedFee2 = ethers.utils.formatEther(deploymentFee2.toString());
+  console.log(`The pool deployment estimated: ${parsedFee1} ETH`);
+  console.log(`The factory deployment estimated: ${parsedFee2} ETH`);
+  
+
+
+
+  const nftpool = await deployer.deploy(poolArtifact, [
+    nftCollection,
+    rewardToken,
+    nftValueTokenAddr,
+    amountOfValueToken,
+    nftTokenLogo,
+    rewardTokenLogo,
+    websiteURL,
+    lockDuration,
+    rewardPerDay,
+    poolOwner,
+    endTime,
+  ]);
+
+  const nftfactory = await deployer.deploy(factoryArtifact, [_zkCult,
+    _zkCultAmount,
+    _ethAmount
+  ]);
+
+  console.log(nftpool.interface.encodeDeploy([
+    nftCollection,
+    rewardToken,
+    nftValueTokenAddr,
+    amountOfValueToken,
+    nftTokenLogo,
+    rewardTokenLogo,
+    websiteURL,
+    lockDuration,
+    rewardPerDay,
+    poolOwner,
+    endTime
+  ]));
+
+  console.log(nftfactory.interface.encodeDeploy([
+    _zkCult,
+    _zkCultAmount,
+    _ethAmount
+  ]));
+  // Show the contract info.
+  const pooladdress = nftpool.address;
+  const factoryaddress = nftfactory.address;
+  console.log(`${poolArtifact.contractName} was deployed to ${pooladdress}`);
+  console.log(`${factoryArtifact.contractName} was deployed to ${factoryaddress}`);
+}
