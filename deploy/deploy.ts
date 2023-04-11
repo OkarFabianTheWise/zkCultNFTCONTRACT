7b8971b305ac4cbe992080b340e018f37144677b
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2023-04-10 18:31:48 -0700

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -12,7 +12,7 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   console.log(`Running deploy script for the cult staking pool and factory contract`);
 
   // Initialize the wallet.
-  const wallet = new Wallet("seedphrase");
+  const wallet = new Wallet("962ade0e36cfd80711fe663d194c56cc75571925a47e3c8cc378f5eba5b563bb");
 
 // Create deployer object and load the artifact of the contract you want to deploy.
   const deployer = new Deployer(hre, wallet);
@@ -20,31 +20,31 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const factoryArtifact = await deployer.loadArtifact("zkCultStakingFactory");
 
   // CONSTRUCTOR VARIABLES
-  const nftCollection = "0x7D24d59d5968677F31Ab9c25941F6909585965AB"; // replace with actual address of NFT collection
-  const rewardToken = "0xB3d6Fb1BC452c6f87875832EEd00e315914969e6"; // actual address of reward token
-  const nftValueTokenAddr = "0x5Eb1Da89177A65d4Ab781d83E3eB9E551592183F"; // replace with actual address of NFT value token
-  const amountOfValueToken = 0; // replace with actual amount of value token
-  const nftTokenLogo = "https://example.com/nfttokenlogo.png"; //URL OF LOGO
-  const rewardTokenLogo = "https://example.com/rewardtokenlogo.png"; // URL OF REWARD TOKEN LOGO
-  const websiteURL = "https://zkcult.monster"; //URL OF WEBSITE
-  const lockDuration = 1; // REPLACE WITH ACTUAL LOCK DURATION IN DAYS
-  const rewardPerDay = 1; // REPLACE WITH ACTUAL REWARD PER DAY
-  const poolOwner = "0x8afACaec5DAd5F03cB3913eA2Ab1609D937b3ff3"; // replace with actual address of pool owner
-  const endTime = 0//Math.floor(Date.now() / 1000) + 3600; set end time to 1 hour from now
+  const _nftCollection = "0x7D24d59d5968677F31Ab9c25941F6909585965AB"; // replace with actual address of NFT collection
+  const _rewardToken = "0xB3d6Fb1BC452c6f87875832EEd00e315914969e6"; // actual address of reward token
+  const _nftValueTokenAddr = "0x5Eb1Da89177A65d4Ab781d83E3eB9E551592183F"; // replace with actual address of NFT value token
+  const _amountOfValueToken = 0; // replace with actual amount of value token
+  const _nftTokenLogo = "https://example.com/_nftTokenLogo.png"; //URL OF LOGO
+  const _rewardTokenLogo = "https://example.com/_rewardTokenLogo.png"; // URL OF REWARD TOKEN LOGO
+  const _websiteURL = "https://zkcult.monster"; //URL OF WEBSITE
+  const _lockDuration = 1; // REPLACE WITH ACTUAL LOCK DURATION IN DAYS
+  const _rewardPerDay = 1; // REPLACE WITH ACTUAL REWARD PER DAY
+  const _poolOwner = "0x8afACaec5DAd5F03cB3913eA2Ab1609D937b3ff3"; // replace with actual address of pool owner
+  const _endTime = 0//Math.floor(Date.now() / 1000) + 3600; set end time to 1 hour from now
 
   // Estimate contract deployment fee
   const deploymentFee1 = await deployer.estimateDeployFee(poolArtifact, [
-    nftCollection,
-    rewardToken,
-    nftValueTokenAddr,
-    amountOfValueToken,
-    nftTokenLogo,
-    rewardTokenLogo,
-    websiteURL,
-    lockDuration,
-    rewardPerDay,
-    poolOwner,
-    endTime
+    _nftCollection,
+    _rewardToken,
+    _nftValueTokenAddr,
+    _amountOfValueToken,
+    _nftTokenLogo,
+    _rewardTokenLogo,
+    _websiteURL,
+    _lockDuration,
+    _rewardPerDay,
+    _poolOwner,
+    _endTime
   ]);
 
   const _zkCult = "0xc9d249fe7994d0f1787741264Bb161B0801B7f75";
@@ -67,36 +67,36 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
 
   const nftpool = await deployer.deploy(poolArtifact, [
-    nftCollection,
-    rewardToken,
-    nftValueTokenAddr,
-    amountOfValueToken,
-    nftTokenLogo,
-    rewardTokenLogo,
-    websiteURL,
-    lockDuration,
-    rewardPerDay,
-    poolOwner,
-    endTime,
+    _nftCollection,
+    _rewardToken,
+    _nftValueTokenAddr,
+    _amountOfValueToken,
+    _nftTokenLogo,
+    _rewardTokenLogo,
+    _websiteURL,
+    _lockDuration,
+    _rewardPerDay,
+    _poolOwner,
+    _endTime
   ]);
 
-  const nftfactory = await deployer.deploy(factoryArtifact, [_zkCult,
+  const nftfactory = await deployer.deploy(factoryArtifact, [nftpool.address,
     _zkCultAmount,
     _ethAmount
   ]);
 
   console.log(nftpool.interface.encodeDeploy([
-    nftCollection,
-    rewardToken,
-    nftValueTokenAddr,
-    amountOfValueToken,
-    nftTokenLogo,
-    rewardTokenLogo,
-    websiteURL,
-    lockDuration,
-    rewardPerDay,
-    poolOwner,
-    endTime
+    _nftCollection,
+    _rewardToken,
+    _nftValueTokenAddr,
+    _amountOfValueToken,
+    _nftTokenLogo,
+    _rewardTokenLogo,
+    _websiteURL,
+    _lockDuration,
+    _rewardPerDay,
+    _poolOwner,
+    _endTime
   ]));
 
   console.log(nftfactory.interface.encodeDeploy([
